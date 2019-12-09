beae81e98275f4f834afaa294b9c34b7a7733ef2
Tran Quoc Hoang Viet <hoangviet1993@gmail.com>
2019-12-09 13:10:05 -0800

    Add various imports and declarations and adjust title text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule
+} from '@angular/material';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { LocationFeedComponent } from './location-feed/location-feed.component';
+import { ConvertDateObjectPipe } from './convert-date-object.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRippleModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        LocationFeedComponent,
+        ConvertDateObjectPipe,
       ],
     }).compileComponents();
   }));
@@ -20,16 +41,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'timezone-locator'`, () => {
+  it(`should have as title 'Current Time Look-up'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('timezone-locator');
+    expect(app.title).toEqual('Current Time Look-up');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('timezone-locator app is running!');
+    expect(compiled.querySelector('.app-title').textContent).toContain(
+      'Current Time Look-up');
   });
 });
