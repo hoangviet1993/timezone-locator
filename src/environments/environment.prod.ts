c18252442209bf54542fbb14d5e108891887e158
Tran Quoc Hoang Viet <hoangviet1993@gmail.com>
2019-12-10 14:59:01 -0800

    Add environment file to keep track of API key, url, etc..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const TIMEZONEDB_API_KEY = "YOUR_API_HERE";
+const TIMEZONEDB_GET_TIME_ZONE_URL = 
+  'https://api.timezonedb.com/v2.1/get-time-zone?key=' + TIMEZONEDB_API_KEY
+  + '&format=json&by=position';
+
 export const environment = {
-  production: true
+  production: true,
+  TIMEZONEDB_GET_TIME_ZONE_URL
 };
