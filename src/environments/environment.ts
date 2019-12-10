612a094c561446f3665f7e5f0ed137365c517853
Viet Tran Quoc Hoang <hoangviet1993@gmail.com>
2019-12-10 15:00:47 -0800

    Refactoring - Move location service code into its own file (#6)
    * Move location service code into its own service file and inject it back to main component
    
    * Add environment file to keep track of API key, url, etc..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const TIMEZONEDB_API_KEY = "YOUR_API_HERE";
+const TIMEZONEDB_GET_TIME_ZONE_URL = 
+  'https://api.timezonedb.com/v2.1/get-time-zone?key=' + TIMEZONEDB_API_KEY
+  + '&format=json&by=position';
 
 export const environment = {
-  production: false
+  production: false,
+  TIMEZONEDB_GET_TIME_ZONE_URL
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
